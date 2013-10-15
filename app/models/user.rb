e3180b2b33c3d058be16c44c8d399dad777095d5
Adan Gutierrez <hevyw8@gmail.com>
2013-10-15 14:44:02 -0500

    Associated users and posts

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :token_authenticatable
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  has_many :posts
+end
--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Bloccit
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -64,5 +65,8 @@ module Bloccit
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Devise instructions
+    config.assets.initialize_on_precompile = false
   end
 end
