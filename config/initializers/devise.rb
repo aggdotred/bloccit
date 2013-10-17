32959f5d59339e3c30a50f4fe0485728ed64011f
Adan Gutierrez <hevyw8@gmail.com>
2013-10-17 15:45:26 -0500

    Configured app for Production emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'confirm@bloccit.com'
+  config.mailer_sender = 'hevyw8@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
