7f48c33bf0abb50843b6f5e0c924ebd693f4d571
Adan Gutierrez <hevyw8@gmail.com>
2013-10-19 16:43:37 -0500

    Installed and implemented Redcarpet w/ extensions: codeblocks, autolink, highlight and quote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def control_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'control-group error'
+    else
+      content_tag :div, capture(&block), class: 'control-group'
+    end
+  end
+
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true, 
+                  autolink: true, 
+                  highlight: true, 
+                  quote: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
+  end
 end
