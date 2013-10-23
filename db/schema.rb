dd110de94433752ba80ef0ce442e05f5b4b604cc
Adan Gutierrez <hevyw8@gmail.com>
2013-10-22 19:45:14 -0500

    fixed addavatar migration using rake:db:redo

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20131020200805) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
